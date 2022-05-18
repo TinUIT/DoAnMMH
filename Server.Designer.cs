01d57fdee37c854ea3c02721a03f7e0b9242dc0e
TinUIT <89720388+TinUIT@users.noreply.github.com>
2022-05-18 14:37:44 +0700

    1437

--- Server.Designer.cs
+++ Server.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DoAnMMH
 {
-    partial class Form1
+    partial class Server
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Server";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
