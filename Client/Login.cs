866b34090f0b404dedc20180da626db20e1eb77c
TinUIT <89720388+TinUIT@users.noreply.github.com>
2022-05-26 21:56:49 +0700

    2156

--- Client/Login.cs
+++ Client/Login.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace Client
 {
@@ -22,12 +23,33 @@ namespace Client
         }
         SocketManager socket = new SocketManager();
 
+        public string SHA256(string plainText)
+        {
+            SHA256 sha256 = new SHA256CryptoServiceProvider();
+
+            sha256.ComputeHash(ASCIIEncoding.ASCII.GetBytes(plainText));
+
+            byte[] result = sha256.Hash;
+            return byteToString(result);
+        }
+
+        public string byteToString(byte[] result)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
+        }
+
         private void btLogin_Click(object sender, EventArgs e)
         {
             if (tbPassword.Text != null && tbUserName.Text != null)
             {
                 socket.ConnectServer();
-                socket.Send(tbUserName.Text + "--" + tbPassword.Text);
+                socket.Send(tbUserName.Text + "--" + SHA256(tbPassword.Text));
             }
         }
 
